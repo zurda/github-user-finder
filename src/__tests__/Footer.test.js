471c77b26edc20f011bfc072b6cd52b2307d3227
Michal Weizman <m.weizman@gmail.com>
2019-01-02 21:12:07 +0000

    Updates

--- src/__tests__/Footer.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { create } from "react-test-renderer";
-import Footer from "../Footer";
-
-test("snapshot", () => {
-  const c = create(<Footer />);
-  expect(c.toJSON()).toMatchSnapshot();
-});
--- src/helpers.js
+++
@@ -1,3 +0,0 @@
-export const addCommas = int => {
-  return int.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-};
